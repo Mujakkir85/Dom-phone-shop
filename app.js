9af02aedee5cd9bb88d42819a047e9f4336060b6
Mujakkir85 <mujakkirlucse@gmail.com>
2022-02-22 19:46:18 +0600

    mutiplication add

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 let getPhoneValue = parseInt(document.getElementById('phone-number').value);
-document.getElementById('phone-plus').addEventListener('click', function () {
 
+
+document.getElementById('phone-plus').addEventListener('click', function () {
     getPhoneValue += 1;
+
+    document.getElementById('update-phone-price').innerText = getPhoneValue * 1200;
     console.log(getPhoneValue)
 })
 
 document.getElementById('phone-minus').addEventListener('click', function () {
-
     getPhoneValue -= 1;
+    document.getElementById('update-phone-price').innerText = getPhoneValue * 1200;
     console.log(getPhoneValue)
 })
 
 
 let getCaseValue = parseInt(document.getElementById('case-number').value)
-document.getElementById('case-plus').addEventListener('click', function () {
 
+document.getElementById('case-plus').addEventListener('click', function () {
     getCaseValue += 1;
+    document.getElementById('update-case-price').innerText = getCaseValue * 50;
     console.log(getCaseValue)
 })
 
 document.getElementById('case-minus').addEventListener('click', function () {
-
     getCaseValue -= 1;
+    document.getElementById('update-case-price').innerText = getCaseValue * 50;
     console.log(getCaseValue)
 })
