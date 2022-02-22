c800d5c77527db2e6fabb9e06ac82ae899d9c5ec
Mujakkir85 <mujakkirlucse@gmail.com>
2022-02-22 20:18:01 +0600

    input number showed

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ let getPhoneValue = parseInt(document.getElementById('phone-number').value);
 
 document.getElementById('phone-plus').addEventListener('click', function () {
     getPhoneValue += 1;
-
+    document.getElementById('phone-number').value = getPhoneValue;
     document.getElementById('update-phone-price').innerText = getPhoneValue * 1200;
-    console.log(getPhoneValue)
+    //console.log(getPhoneValue)
 })
 
 document.getElementById('phone-minus').addEventListener('click', function () {
     getPhoneValue -= 1;
+    document.getElementById('phone-number').value = getPhoneValue;
     document.getElementById('update-phone-price').innerText = getPhoneValue * 1200;
-    console.log(getPhoneValue)
+    //console.log(getPhoneValue)
 })
 
 
@@ -19,12 +20,14 @@ let getCaseValue = parseInt(document.getElementById('case-number').value)
 
 document.getElementById('case-plus').addEventListener('click', function () {
     getCaseValue += 1;
+    document.getElementById('case-number').value = getCaseValue;
     document.getElementById('update-case-price').innerText = getCaseValue * 50;
-    console.log(getCaseValue)
+    //console.log(getCaseValue)
 })
 
 document.getElementById('case-minus').addEventListener('click', function () {
     getCaseValue -= 1;
+    document.getElementById('case-number').value = getCaseValue;
     document.getElementById('update-case-price').innerText = getCaseValue * 50;
-    console.log(getCaseValue)
+    //console.log(getCaseValue)
 })
